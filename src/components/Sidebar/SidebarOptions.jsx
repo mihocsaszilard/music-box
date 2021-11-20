15c96fc86d2dedab649eed95e9d199e287d42495
Mihocsa Szilard <mihocsa48@gmail.com>
2021-11-20 16:33:29 +0100

    get playlistTracks

--- src/components/Sidebar/SidebarOptions.jsx
+++ src/components/Sidebar/SidebarOptions.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { useDataLayerValue } from "../../data/DataLayer";
 import "./SidebarOptions.scss";
 
-export default function SidebarOptions({ title, Icon }) {
+export default function SidebarOptions({ spotify, id, title, Icon }) {
+  const [{}, dispatch] = useDataLayerValue();
+
+  const changePlaylist = (id, e) => {
+    dispatch({
+      type: "SET_CURRENT_PLAYLIST",
+      id: id,
+    });
+
+    spotify.getPlaylistTracks(id).then((resp) => {
+      dispatch({
+        type: "SET_TRACKS",
+        tracks: resp,
+      });
+    });
+  };
+
   return (
     <div className="sidebarOption">
       {Icon && <Icon className="sidebarOption_icon" />}
-      {Icon ? <h4>{title} </h4> : <p>{title}</p>}
+      {Icon ? (
+        <h4>{title} </h4>
+      ) : (
+        <p onClick={(e) => changePlaylist(id, e)}> {title}</p>
+      )}
     </div>
   );
 }
