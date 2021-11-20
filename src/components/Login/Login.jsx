61e576a1eb73a1a2e5ed3a03e46092a5e36e6b7f
Mihocsa Szilard <mihocsa48@gmail.com>
2021-11-20 13:45:56 +0100

    change login image

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,8 +6,8 @@ function Login() {
   return (
     <div className="login">
       <img
-        src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
-        alt="Spotify logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
       />
       <a href={loginUrl}>Login with Spotify</a>
     </div>
