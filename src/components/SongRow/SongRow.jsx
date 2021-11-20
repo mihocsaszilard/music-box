a20893025738b5378a0f70024701c3d18ecfb60b
Mihocsa Szilard <mihocsa48@gmail.com>
2021-11-20 16:34:09 +0100

    play music onClick

--- src/components/SongRow/SongRow.jsx
+++ src/components/SongRow/SongRow.jsx
@@ -1,16 +1,69 @@
 import React from "react";
 import "./SongRow.scss";
+import { useDataLayerValue } from "../../data/DataLayer";
+import { useSoundLayerValue } from "../../data/SoundLayer";
 
 export default function SongRow({ track }) {
+  const [{}, dispatch] = useDataLayerValue();
+  const [{ playing, repeat }, soundDispatch] = useSoundLayerValue();
+
+  const startPlaying = () => {
+    soundDispatch({
+      type: "SET_PLAYING",
+      playing: true,
+    });
+  };
+
+  const stopPlaying = () => {
+    soundDispatch({
+      type: "SET_PLAYING",
+      playing: false,
+    });
+  };
+
+  const changeTrack = (e, track) => {
+    dispatch({
+      type: "SET_TRACK",
+      track: track,
+    });
+
+    let wasPlaying = playing;
+    soundDispatch({
+      type: "SET_PLAYING",
+      playing: false,
+    });
+
+    let audio = new Audio(track.preview_url);
+    audio.loop = repeat;
+    soundDispatch({
+      type: "SET_AUDIO",
+      audio: audio,
+    });
+
+    if (wasPlaying) {
+      soundDispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    }
+    document.title = `${track.name} - ${track.artists
+      .map((artist) => artist.name)
+      .join(", ")}`;
+  };
+
   return (
-    <div className="songRow">
+    <div
+      className="songRow"
+      onClick={(e) => {
+        changeTrack(e, track);
+        startPlaying();
+      }}
+    >
       <img className="songRow_album" src={track.album.images[0].url} alt="" />
       <div className="songRow_info">
         <h1>{track.name}</h1>
-        <p>
-          {track.artists.map((artist) => artist.name).join(", ")} -{" "}
-          {track.album.name}{" "}
-        </p>
+        <p>{track.artists.map((artist) => artist.name).join(", ")} </p>
+        <p>{track.album.name} </p>
       </div>
     </div>
   );
